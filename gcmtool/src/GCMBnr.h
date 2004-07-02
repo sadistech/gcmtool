9e8929b1c2ba6498b0b68a0f0e24810c39e6688a
Spike Grobstein <me@spike.cx>
2004-07-02 22:07:08 +0000

    fixed a bug- was writing as an int instead of a char into a string (multi-digit instead of single)

--- gcmtool/src/GCMBnr.h
+++ gcmtool/src/GCMBnr.h
@@ -68,7 +68,6 @@ GCMBnrStruct *GCMRawBnrToStruct(char *raw);
 void GCMBnrStructToRaw(GCMBnrStruct *b, char *buf);
 
 //color conversion
-static uchar GCMBnrReverseBits(uchar v, int bitCount);
 GCMRgbColor *GCMRGB5A1toColor(u16 s);
 u16 GCMColorToRGB5A1(GCMRgbColor *c);
 
