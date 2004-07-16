ec9a28b9904e95c7a58b5838aa9b0c51a9e9ce2d
Spike Grobstein <me@spike.cx>
2004-07-16 17:08:41 +0000

    fixed a bug introduced when fixing the lvalue compiler warnings... all values now load properly

--- gcmtool/src/GCMDiskHeader.c
+++ gcmtool/src/GCMDiskHeader.c
@@ -157,8 +157,9 @@ GCMDiskHeaderStruct *GCMRawDiskHeaderToStruct(char *rawHeader) {
 	
 	rawHeader += 18; //skip padding ?
 
-	h->unknown1 = *(u32*)rawHeader++;
+	h->unknown1 = *((u32*)rawHeader)++;
 	h->unknown1 = ntohl(h->unknown1);
+	//((u32*)rawHeader)++;
 	
 	bzero(h->gameName, GCM_GAME_NAME_LENGTH);
 	memcpy(h->gameName, rawHeader, GCM_GAME_NAME_LENGTH);
@@ -167,30 +168,30 @@ GCMDiskHeaderStruct *GCMRawDiskHeaderToStruct(char *rawHeader) {
 	h->debugMonitorOffset = *((u32*)rawHeader)++;
 	h->debugMonitorOffset = ntohl(h->debugMonitorOffset);
 	
-	h->debugMonitorAddress = *((u32*)rawHeader);
+	h->debugMonitorAddress = *((u32*)rawHeader)++;
 	h->debugMonitorAddress = ntohl(h->debugMonitorAddress);
 	
-	rawHeader += 28; //skip some padding?
+	rawHeader += 24; //skip some padding?
 	
-	h->dolOffset	= *(u32*)rawHeader++;
+	h->dolOffset	= *((u32*)rawHeader)++;
 	h->dolOffset	= ntohl(h->dolOffset);
 	
-	h->fstOffset	= *(u32*)rawHeader++;
+	h->fstOffset	= *((u32*)rawHeader)++;
 	h->fstOffset	= ntohl(h->fstOffset);
 	
-	h->fstSize		= *(u32*)rawHeader++;
+	h->fstSize		= *((u32*)rawHeader)++;
 	h->fstSize		= ntohl(h->fstSize);
 	
-	h->fstSizeMax   = *(u32*)rawHeader++;
+	h->fstSizeMax   = *((u32*)rawHeader)++;
 	h->fstSizeMax	= ntohl(h->fstSizeMax);
 	
-	h->userPosition = *(u32*)rawHeader++;
+	h->userPosition = *((u32*)rawHeader)++;
 	h->userPosition = ntohl(h->userPosition);
 	
-	h->userLength   = *(u32*)rawHeader++;
+	h->userLength   = *((u32*)rawHeader)++;
 	h->userLength	= ntohl(h->userLength);
 	
-	h->unknown2		= *(u32*)rawHeader++;
+	h->unknown2		= *((u32*)rawHeader)++;
 	h->unknown2		= ntohl(h->unknown2);
 	
 	return h;
